58e5e2c7ffdf3a518cae8080bd88bd1b6cad39e4
Meireles <junior_turren@hotmail.com>
2023-09-04 20:30:21 -0300

    projeto-pokedex

---
+++ assets/js/poke-api.js
@@ -0,0 +1,53 @@
+
+const pokeApi = {}
+
+function convertPokeApiDetailToPokemon(pokeDetail){
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+    
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
+pokeApi.getPokemonsDetail = (pokemon) =>{
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then((convertPokeApiDetailToPokemon))
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 20) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+    return fetch(url)
+        .then((response) => response.json()) //convertendo lista pra json
+        .then((jsonBody) => jsonBody.results) //
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonsDetail))//lista de promessa do detalhe do pokemon em json
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
+        }
+        //.catch((error) => console.error(error))
+
+
+// Promise.all([
+//     fetch('https://pokeapi.co/api/v2/pokemon/1'),
+//     fetch('https://pokeapi.co/api/v2/pokemon/2'),
+//     fetch('https://pokeapi.co/api/v2/pokemon/3'),
+//     fetch('https://pokeapi.co/api/v2/pokemon/4')
+// ]).then((results)=>{
+//     console.log(results)
+// })
+
+
+
+
+
+
+
